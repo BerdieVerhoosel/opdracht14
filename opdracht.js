506ae0e1d14baf5a539aeb8ea0691133ed934796
BerdieVerhoosel <91963456+BerdieVerhoosel@users.noreply.github.com>
2021-12-13 16:21:41 +0100

    Add files via upload

---
+++ opdracht.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'Berdie',
+    age: 30
+};
+
+console.log(person);
+
+console.log(person.name);
+
+console.log(person.age);
+
+person.name = 'Paul';
+
+console.log(person.name);
+
